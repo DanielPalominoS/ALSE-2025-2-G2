81e3ff739badcf06fdb5be4e023bb6a0f503be99
Daniel Palomino Suarez <45025812+DanielPalominoS@users.noreply.github.com>
2025-09-05 13:52:06 +0000

    showing addresses

--- example1/ejemplo1.cpp
+++ example1/ejemplo1.cpp
@@ -44,7 +44,21 @@ int main()
         std::cout<<"x: "<<x<<std::endl;
     }
     aux_fun();
-    //std::cout<<"a: "<<a<<std::endl;   
-
+    //std::cout<<"a: "<<a<<std::endl;
+    std::cout<<"sizeof global var: "<<sizeof(decimalLargeNumber)<<std::endl;
+    std::cout<<"sizeof Double var: "<<sizeof(pi)<<std::endl;//printf("Double var: %f\n",pi)
+    std::cout<<"sizeof Float var: "<<sizeof(height)<<std::endl;
+    std::cout<<"sizeof Char var: "<<sizeof(character)<<std::endl;
+    std::cout<<"sizeof bool var: "<<sizeof(flag)<<std::endl;
+    std::cout<<"sizeof short var: "<<sizeof(count)<<std::endl;
+    std::cout<<"sizeof unsinged long var: "<<sizeof(largeInteger)<<std::endl;
+    
+    std::cout<<"global var address: "<<&decimalLargeNumber<<std::endl;
+    std::cout<<"Double var address: "<<&pi<<std::endl;//printf("Double var: %f\n",pi)
+    std::cout<<"Float var address: "<<&height<<std::endl;
+    std::cout<<"Char var address: "<<&character<<std::endl;
+    std::cout<<"bool var address: "<<&flag<<std::endl;
+    std::cout<<"short var address: "<<&count<<std::endl;
+    std::cout<<"unsinged long var address: "<<&largeInteger<<std::endl;
     return 0;
 }
